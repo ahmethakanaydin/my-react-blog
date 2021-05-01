761c296ab35591487be4afa2d518bdbbe2afc627
Ahmet Aydın <ahmet@hakanaydin.me>
2021-05-01 19:56:33 +0300

    mobile view improvement

--- src/components/MainBox.js
+++ src/components/MainBox.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import { Col, Container } from 'react-bootstrap'
 
 function MainBox() {
     return (
+        
             <div className="box">
                 <h1 className="home-text"><span>Merhaba.</span>Ben Ahmet</h1>
                 <div className="slogan">Tutkulu bir Front End Geliştiricisi</div>
             </div>
+        
+            
     )
 }
 
