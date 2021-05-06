5d157ba44562d6ea53efca3ec5fe148963fe3660
Ahmet Aydın <ahmet@hakanaydin.me>
2021-05-06 13:38:16 +0300

    bio updated

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -10,8 +10,9 @@ const ImageCard = () => {
             <Col className="mt-3 mb-5" xs={12} >
                 <Image className="avatar mb-2" src={Avatar} roundedCircle thumbnail />
                 <h5 className="text-center">Ahmet Hakan Aydın</h5>
-                <span className="text-center"><p>Beykent Üniversitesi <br></br>Bilgisayar Mühendisliği 3.Sınıf</p></span>
-                <p className="text-center"><a href="mailto:ahmet@hakanaydin.me">ahmet@hakanaydin.me</a></p>
+                <span className="text-center">
+                    <p>JR. Frontend Developer <br></br><a href="mailto:ahmet@hakanaydin.me">ahmet@hakanaydin.me</a></p>
+                </span>
                 <div className="text-center"><Social /></div>
                 <hr></hr>
             </Col>
