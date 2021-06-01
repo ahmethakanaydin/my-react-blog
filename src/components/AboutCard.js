ee34c577760838e7a087029a662ca98958b6fb4b
Ahmet Aydın <ahmet@hakanaydin.me>
2021-06-01 14:03:58 +0300

    mobile view improved

--- src/components/AboutCard.js
+++ src/components/AboutCard.js
@@ -19,7 +19,7 @@ const AboutCard = () => {
                     öğrencisiyim. Uzun zamandan beri web alanına ilgiliyim. <a className="text-decoration-none" href="https://gelecegiyazanlar.turkcell.com.tr/" target="_blank" rel="noreferrer">Turkcell Geleceği Yazanlar</a>'da HTML,     CSS ve Javascript giriş bölümlerini tamamladıktan sonra sınavlarda da başarılı olup sertifika kazandım. Üniversite eğitimimde öğrendiğim Matlab, C, C++ ve C# dillerinin yanında kendi  çabalarım ile öğrediğim Python dilinde kendi çapımda projeler geliştirdim. En son olarak <a className="text-decoration-none" href="https://kodluyoruz.org" target="_blank" rel="noreferrer">Kodluyoruz</a>'un     bünyesinde olan <a href="https://patika.dev" target="_blank" rel="noreferrer">Patika</a>'da Frontend Web Development Patikası'nı tamamladım.
                 </p>
             <p className='mt-4'>
-                Yazılımın yanı sıra müzik ile ilgileniyorum. Dinlemek isterseniz youtube kanal <a href="https://www.youtube.com/channel/UCozyfcHE4abS06yP2lkvSGA" target="_blank" rel="noreferrer">linkimi </a> bırakıyorum :)
+                Yazılımın yanı sıra müzik ile ilgileniyorum. Dinlemek isterseniz Youtube kanal <a href="https://www.youtube.com/channel/UCozyfcHE4abS06yP2lkvSGA" target="_blank" rel="noreferrer">linkimi </a> bırakıyorum :)
             </p>
             <a className='btn btn-outline-primary' href={cv} target="_blank" rel="noreferrer">CV İndir</a>
             <hr></hr>
